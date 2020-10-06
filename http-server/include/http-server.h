b68a9576e972b13085e9140f72aaded262ab5be9
master-spartacus <65410851+master-spartacus@users.noreply.github.com>
2020-10-06 11:04:55 +0530

    Update http-server.h

--- http-server/include/http-server.h
+++ http-server/include/http-server.h
@@ -9,27 +9,34 @@
 #include <string.h>
 #include <netinet/in.h>
 
+//  Macro function for used for error handling
 #define err_exit(msg) { \
                       perror(msg); \
                       exit(-1); \
                       }
 
+//  Max 10 connections are accepted in listen
 #define BACKLOG 10
+
+//  Buffer size
 #define SIZE 1024
+
+//  Response for invalid header
 #define BAD_REQUEST "HTTP/1.1 400 Bad Request\nContent-Type: text/plain\n \
                       Content-Length: 16\n\n400 Bad Request!"
+
+//  Response for file not found case
 #define FILE_NOT_FOUND "HTTP/1.1 404 File Not Found\nContent-Type: \
                       text/plain\nContent-Length: 19\n\n404 File Not Found!"
 
-// HttpHeader structure will store first line of the request.
-
+//  HttpHeader structure will store first line of the request.
 typedef struct HttpHeader {
   char  *type;
   char  *filename;
   char  *connection;
 }HttpHeader;
 
-
+//  Parses the first line of the request header and creates response.
 void ParseHttpHeaderAndConstructResponse(char *header, char *Response);
 
-#endif 
+#endif //  HTTP_SERVER_HEADER
